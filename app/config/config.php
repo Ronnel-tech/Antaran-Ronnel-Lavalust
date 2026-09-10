b279792251634bb2a8b87ac5c786567412786425
Ronnel <logdatronnell@gmail.com>
2026-09-10 22:33:05 +0800

    sfsdgsrjyds

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['environment'] = getenv('APP_ENV') ?: 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= getenv('APP_URL') ?: 'http://localhost:3000/';
+$config['base_url'] 				= '';
 
 /*
 |--------------------------------------------------------------------------
@@ -99,7 +99,7 @@ $config['proxy_enabled']           = FALSE;
 | variable to blank.
 |
 */
-$config['index_page']               = 'index.php';
+$config['index_page']               = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -235,7 +235,7 @@ $config['sess_driver']             = 'file';
 $config['sess_table']              = 'sessions';
 $config['sess_cookie_name']        = 'LLSession';
 $config['sess_expiration']         = 7200;
-$config['sess_save_path']          = ROOT_DIR . 'runtime/session';
+$config['sess_save_path']          = '';
 $config['sess_match_ip']           = FALSE;
 $config['sess_match_fingerprint']  = FALSE;
 $config['sess_time_to_update']     = 300;
@@ -263,10 +263,7 @@ $config['session_hmac_secret']     = getenv('APP_KEY') ?: '';
 $config['cookie_prefix']           = '';
 $config['cookie_domain']           = '';
 $config['cookie_path']             = '/';
-$config['cookie_secure']           = filter_var(
-    getenv('COOKIE_SECURE') ?: ($config['environment'] === 'production'),
-    FILTER_VALIDATE_BOOLEAN
-);
+$config['cookie_secure']           = FALSE;
 $config['cookie_expiration']       = 86400;
 $config['cookie_httponly']         = FALSE;
 $config['cookie_samesite']         = 'Strict';
